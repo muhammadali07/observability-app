f9678aa61a18f3547368bafdd33f2ded0a9ba82b
Muh Ali Bakhtiar <muhalibakhtiar@gmail.com>
2024-04-24 23:11:27 +0700

    fixing main.go

--- app/main.go
+++ app/main.go
@@ -9,12 +9,10 @@ import (
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
-
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
 	"go.opentelemetry.io/otel/sdk/resource"
-	oteltrace "go.opentelemetry.io/otel/sdk/trace"
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
@@ -28,35 +26,24 @@ var (
 func init() {
 	otlpEndpoint = os.Getenv("OTLP_ENDPOINT")
 	if otlpEndpoint == "" {
-		// log.Fatalln("You MUST set OTLP_ENDPOINT env variable!")
-		otlpEndpoint = "localhost:4318 "
+		otlpEndpoint = "localhost:4318"
 	}
-
 }
 
-// List of supported exporters
-// https://opentelemetry.io/docs/instrumentation/go/exporters/
-
-// Console Exporter, only for testing
-func newConsoleExporter() (oteltrace.SpanExporter, error) {
+// newConsoleExporter creates a new exporter for testing, outputting to the console.
+func newConsoleExporter() (sdktrace.SpanExporter, error) {
 	return stdouttrace.New()
 }
 
-// OTLP Exporter
-func newOTLPExporter(ctx context.Context) (oteltrace.SpanExporter, error) {
-	// Change default HTTPS -> HTTP
+// newOTLPExporter creates a new OTLP exporter with the specified context.
+func newOTLPExporter(ctx context.Context) (sdktrace.SpanExporter, error) {
 	insecureOpt := otlptracehttp.WithInsecure()
-
-	// Update default OTLP reciver endpoint
 	endpointOpt := otlptracehttp.WithEndpoint(otlpEndpoint)
-
 	return otlptracehttp.New(ctx, insecureOpt, endpointOpt)
 }
 
-// TracerProvider is an OpenTelemetry TracerProvider.
-// It provides Tracers to instrumentation so it can trace operational flow through a system.
+// newTraceProvider creates a new TracerProvider with the specified exporter.
 func newTraceProvider(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
-	// Ensure default SDK resources and the required service name are set.
 	r, err := resource.Merge(
 		resource.Default(),
 		resource.NewWithAttributes(
@@ -64,9 +51,8 @@ func newTraceProvider(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
 			semconv.ServiceName("myapp"),
 		),
 	)
-
 	if err != nil {
-		panic(err)
+		log.Fatalf("failed to create resource: %v", err)
 	}
 
 	return sdktrace.NewTracerProvider(
@@ -77,51 +63,35 @@ func newTraceProvider(exp sdktrace.SpanExporter) *sdktrace.TracerProvider {
 
 func main() {
 	ctx := context.Background()
-
-	// For testing to print out traces to the console
-	// exp, err := newConsoleExporter()
 	exp, err := newOTLPExporter(ctx)
-
 	if err != nil {
 		log.Fatalf("failed to initialize exporter: %v", err)
 	}
 
-	// Create a new tracer provider with a batch span processor and the given exporter.
 	tp := newTraceProvider(exp)
-
-	// Handle shutdown properly so nothing leaks.
 	defer func() { _ = tp.Shutdown(ctx) }()
 
 	otel.SetTracerProvider(tp)
-
-	// Finally, set the tracer that can be used for this package.
 	tracer = tp.Tracer("myapp")
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-
 	r.Get("/devices", getDevices)
-	http.ListenAndServe(":8080", r)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
 
 func getDevices(w http.ResponseWriter, r *http.Request) {
 	ctx, span := tracer.Start(r.Context(), "HTTP GET /devices")
 	defer span.End()
 
-	// Simulate Database call to fetch connected devices.
 	db(ctx)
-
-	// Add additional delay to simulate HTTP request.
 	time.Sleep(1 * time.Second)
-
-	// Return devices
 	w.Write([]byte("ok"))
 }
 
 func db(ctx context.Context) {
 	_, span := tracer.Start(ctx, "SQL SELECT")
 	defer span.End()
-
-	// Simulate Database call to SELECT connected devices.
 	time.Sleep(2 * time.Second)
 }
